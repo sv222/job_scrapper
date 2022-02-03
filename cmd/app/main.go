14aab8c785150d5846c3acae51a27fc025a902d6
sv222 <sv_golang@mail.ru>
2022-02-03 15:47:58 +0100

    add dockerfile / path join

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,27 +6,36 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 )
 
-type Job struct {
-	Title string
-	Link  string
-}
+// TODO:
+// 1. Make filtering final output file without duplicates.
+// 2. Add new source, not only profesia.sk.
+// 3. Implement html template instead of txt (to be able to follow links).
 
 func main() {
 
-	//ticker := time.NewTicker(8 * time.Hour)
-	//
-	//// for every `tick` that our `ticker`
-	//// emits, we print `tock`
-	//for _ = range ticker.C {
-	//	app.Parse()
-	//}
+	path := filepath.Join("data", "log.txt")
 
+	file, err := os.Create(path)
+	if err != nil {
+		log.Println("couldn't create log file'")
+	}
+
+	defer file.Close()
+
+	// make first fetching data
 	app.Parse()
 
+	// iterate scrapping every N hours.
+	go parseAgain(8*time.Hour, app.Parse)
+
+	// handle requests for index page
 	http.HandleFunc("/", index)
 
+	// starting server
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -44,5 +53,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("couldn't read log file'")
 	}
 
-	fmt.Fprint(w, data)
+	fmt.Fprint(w, string(data))
+}
+
+// parseAgain function makes iteration for a specific function
+func parseAgain(d time.Duration, f func()) {
+	for _ = range time.Tick(d) {
+		f()
+	}
 }
